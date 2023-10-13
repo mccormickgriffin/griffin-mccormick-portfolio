32bd7fb50b6896ed8161590a4fc60e21a447aca3
Griffin McCormick <mccormickgriffin@gmail.com>
2023-10-13 15:51:49 -0400

    added rough draft for about me section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,7 +3,13 @@ import "styles/AboutMe.scss";
 function AboutMe() {
   return (
     <div>
-      About Me Placeholder
+      <h1>
+        Hi! My name is Griffin and I am a full stack web developer 
+      </h1>
+      <p>
+        I love all things technology from developing web and mobile application to playing video games.
+        I have always been passionate about both learning and teaching software development techniques and principles.
+      </p>
     </div>
   );
 }
