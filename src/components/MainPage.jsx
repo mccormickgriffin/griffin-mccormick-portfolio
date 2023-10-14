c30093970f2aa725f54054ee8c8440f2f88ee2c2
Griffin McCormick <mccormickgriffin@gmail.com>
2023-10-14 19:50:06 -0400

    hide left nav for now

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,12 +1,12 @@
 import "styles/MainPage.scss";
-import LeftNav from "components/LeftNav";
+// import LeftNav from "components/LeftNav";
 import MainPageContent from "components/MainPageContent";
 
 
 function MainPage() {
   return (
     <div className="main-page">
-      <LeftNav/>
+      {/* <LeftNav/> */}
       <MainPageContent/>
     </div>
   );
--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,11 +1,17 @@
 import "styles/TopNav.scss";
 import Logo from "assets/griffinLogo.svg";
+import { NAV_ITEMS } from "lib/consts";
 
 function TopNav() {
   return (
-    <nav>
+    <nav className="top-nav">
       <div className="left-side">
         <img src={Logo} alt="Logo" />
+      </div>
+      <div className="right-side">
+        {NAV_ITEMS.map((navItem) => (
+          <button type="button">{navItem.label}</button>
+        ))}
       </div>
     </nav>
   );
