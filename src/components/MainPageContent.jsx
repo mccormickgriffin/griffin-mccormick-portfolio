190c196704645c36c131301ff9b39641031c99de
Griffin McCormick <mccormickgriffin@gmail.com>
2023-10-15 17:45:17 -0400

    make all sections the same height

--- src/components/MainPageContent.jsx
+++ src/components/MainPageContent.jsx
@@ -1,4 +1,4 @@
-import "styles/MainPage.scss";
+import "styles/MainPageContent.scss";
 import AboutMe from "./AboutMe";
 import Experience from "./Experience";
 import MiniProjects from "./MiniProjects";
