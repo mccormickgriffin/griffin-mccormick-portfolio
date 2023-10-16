08bc58639e64478464d8125a15e7d521b2a076cc
Griffin McCormick <mccormickgriffin@gmail.com>
2023-10-16 17:12:35 -0400

    fully removed left nav

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,12 +1,9 @@
 import "styles/MainPage.scss";
-// import LeftNav from "components/LeftNav";
 import MainPageContent from "components/MainPageContent";
-
 
 function MainPage() {
   return (
     <div className="main-page">
-      {/* <LeftNav/> */}
       <MainPageContent/>
     </div>
   );
