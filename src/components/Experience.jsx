27e1dcb07cc08c140e77551229000acfeffdc61d
Griffin McCormick <mccormickgriffin@gmail.com>
2023-10-21 12:16:55 -0400

    added ability to download my resume

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -5,6 +5,15 @@ function Experience() {
   return (
     <section id={SECTION_IDS.EXPERIENCE}>
       Experience Placeholder
+      <div>
+        See my full resume&nbsp;
+        <a
+          href={process.env.PUBLIC_URL + "/assets/resume.pdf"}
+          download="resume.pdf"
+        >
+          here
+        </a>
+      </div>
     </section>
   );
 }
