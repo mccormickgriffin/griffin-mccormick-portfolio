b9719ec32fb486ef5b1e2a5a7f4def597e883c8d
Griffin McCormick <mccormickgriffin@gmail.com>
2023-10-30 18:02:05 -0400

    added very simple contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,12 @@
 import "styles/Contact.scss";
 import { SECTION_IDS } from "lib/consts";
+import ContactForm from "./ContactForm";
 
 function Contact() {
   return (
     <section id={SECTION_IDS.CONTACT}>
-      Contact Placeholder
+      <h1>Contact</h1>
+      <ContactForm />
     </section>
   );
 }
---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+const ContactForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleMessageChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Message:", message);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>
+          Name:
+          <input type="text" value={name} onChange={handleNameChange} />
+        </label>
+      </div>
+      <div>
+        <label>
+          Email:
+          <input type="email" value={email} onChange={handleEmailChange} />
+        </label>
+      </div>
+      <div>
+        <label>
+          Message:
+          <textarea value={message} onChange={handleMessageChange} />
+        </label>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default ContactForm;
